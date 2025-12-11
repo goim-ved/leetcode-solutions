c9d7d1b0114e57b99b095a354ae06cc60cac405b
goim-ved <vedantgoim1@gmail.com>
2025-12-11 22:08:48 +0530

    python twoSum

---
+++ python/twoSum.py
@@ -0,0 +1,13 @@
+def twoSum(nums: list, target: int):
+    seen = {}
+    for i, num in enumerate(nums):
+        diff = target - num
+        if diff in seen:
+            return [seen[diff], i]
+        seen[num] = i
+    return None
+
+nums = [2,3,4,5]
+target = 9
+
+print(twoSum(nums, target))
