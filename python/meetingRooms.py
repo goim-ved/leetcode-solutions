f185d36e557c11e1b47a4e2232ebba6780432f8d
goim-ved <vedantgoim1@gmail.com>
2025-12-14 17:28:26 +0530

    day two done

---
+++ python/meetingRooms.py
@@ -0,0 +1,11 @@
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+        
+def meetingRooms(intervals: list[Interval]) -> bool:
+    intervals.sort(key=lambda i: i.start)
+    for i in range(len(intervals) - 1):
+        if intervals[i].end > intervals[i+1].start:
+            return False
+    return True
---
+++ python/validPalindrome.py
@@ -0,0 +1,16 @@
+def isValidPalindrome(s: str) -> bool:
+    l, r = 0, len(s) - 1
+    while l < r:
+        if not s[l].isalnum():
+            l += 1
+        elif not s[r].isalnum():
+            r -= 1
+        elif s[l].lower() != s[r].lower():
+            return False
+        else:
+            l += 1
+            r -= 1
+    return True
+
+name = "#Jac.aj*"
+print(isValidPalindrome(name))
