bd010abae4846b21f8f9aa6ec79a6bc03b852ac2
goim-ved <vedantgoim1@gmail.com>
2025-12-17 16:29:53 +0530

    added buy and sell stocks

---
+++ python/buynsellStock.py
@@ -0,0 +1,12 @@
+def maxProfit(prices: list[int]) -> int:
+    min_price = float('inf')
+    max_profit = 0
+
+    for price in prices:
+        if price < min_price:
+            min_price = price
+        else:
+            profit = price - min_price
+            if profit > max_profit:
+                max_profit = profit
+    return max_profit
